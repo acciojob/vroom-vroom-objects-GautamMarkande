5814fc3011ae2ded483a8b5172a3630e0ddff5fb
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-19 11:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+function Car(make, model) {
+	this.make=make,
+	this.model=model
+}
+const car1 = new Car('goli','a5');
+car1.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model}`;
+}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed = topSpeed
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar 
+const sport1 = new SportsCar('goli','a5',100);
 // Do not change the code below
+
+sport1.prototype.getTopSpeed=function(){
+	return `${this.topSpeed}`
+}
 window.Car = Car;
 window.SportsCar = SportsCar;
